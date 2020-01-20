57f09aa946a1f028768ada5540e4510d7a8247ed
tamaszentai <zentaitamas@gmail.com>
2020-01-20 18:12:51 +0000

    getters for sportsteam

--- specs/sportsteam_spec.rb
+++ specs/sportsteam_spec.rb
@@ -5,14 +5,27 @@ MiniTest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSportsteam < MiniTest::Test
 
-
-
-
   def setup
     @sportsteam = Sportsteam.new("Eagles", ["Bela Kovacs", "Istvan Puskas", "Ferenc Toth"], "Balazs Danyi")
     @sportsteam2 = Sportsteam.new("Grizzlys", ["Tamas Zentai", "Tamas Barany", "Daniel Pinter"], "Lajos Rajna")
     @sportsteam3 = Sportsteam.new("Bulls", ["Tamas Servanszki", "Bettina Zentai", "Csanad Fekete"], "Lajos Agoston")
   end
 
-  
+  def test_get_team_name
+    result = @sportsteam.get_team_name
+    assert_equal("Eagles", result)
+  end
+
+  def test_get_players
+    result = @sportsteam2.get_players
+    assert_equal(["Tamas Zentai", "Tamas Barany", "Daniel Pinter"], result)
+  end
+
+  def test_get_coach
+    result = @sportsteam3.get_coach
+    assert_equal("Lajos Agoston", result)
+  end
+
+
+
 end
--- sportsteam.rb
+++ sportsteam.rb
@@ -3,10 +3,19 @@ class Sportsteam
     @team_name = team_name
     @players = players
     @coach = coach
-    @points= 0
   end
 
+  def get_team_name
+    return @team_name
+  end
 
+  def get_players
+    return @players
+  end
+
+  def get_coach
+    return @coach
+  end
 
 
 end
